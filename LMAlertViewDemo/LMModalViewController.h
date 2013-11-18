1da4a7d6f19d5225b032c1827f121bdb821aaa1a
Lee McDermott <github@leemcdermott.co.uk>
2013-11-18 18:57:13 +0700

    Add view controller class to handle cancel button

---
+++ LMAlertViewDemo/LMModalViewController.h
@@ -0,0 +1,13 @@
+//
+//  LMModalViewController.h
+//  LMAlertViewDemo
+//
+//  Created by Lee McDermott on 17/11/2013.
+//  Copyright (c) 2013 Bestir Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LMModalViewController : UIViewController
+
+@end
