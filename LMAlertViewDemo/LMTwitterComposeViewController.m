9852d44edfb1a12d8201d09b0879c1cbafb1b785
Lee McDermott <github@leemcdermott.co.uk>
2013-12-10 16:46:38 +0700

    Simplify horizontal line view

--- LMAlertViewDemo/LMTwitterComposeViewController.m
+++ LMAlertViewDemo/LMTwitterComposeViewController.m
@@ -22,15 +22,12 @@
 {
     [super viewDidLoad];
 	
-	UIView *lineView = [[UIView alloc] initWithFrame:CGRectMake(0.0, 137.0, self.view.frame.size.width, 1.0)];
-	lineView.autoresizingMask = UIViewAutoresizingFlexibleWidth;
 	
-	UIView *lineViewInner = [[UIView alloc] initWithFrame:CGRectMake(0.0, 0.5, self.view.frame.size.width, 0.5)];
+	
+	UIView *lineView = [[UIView alloc] initWithFrame:CGRectMake(0.0, 137.5, self.view.frame.size.width, 0.5)];
 	// This is the default UITableView separator color
-	lineViewInner.backgroundColor = [UIColor colorWithHue:360/252 saturation:0.02 brightness:0.80 alpha:1];
-	lineViewInner.autoresizingMask = UIViewAutoresizingFlexibleWidth;
-	
-	[lineView addSubview:lineViewInner];
+	lineView.backgroundColor = [UIColor colorWithHue:360/252 saturation:0.02 brightness:0.80 alpha:1];
+	lineView.autoresizingMask = UIViewAutoresizingFlexibleWidth;
 	
 	[self.headerView addSubview:lineView];
 	
