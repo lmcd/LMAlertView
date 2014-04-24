7189b44e3191ae12f7aea1fa528871a182308aec
Lin Cheng Kai <whitefur@domiso.cc>
2014-04-24 17:44:19 +0800

    Add a UIDevice category for determine current system version float value.  There are two rows of code using “window.tintColor = ....”, and window.tintColor is new feature begin iOS 7.  I inserted a version condition by UIDevice category before using window.tintColor.

---
+++ LMAlertView/UIDevice+SystemVersionNumber.h
@@ -0,0 +1,14 @@
+//
+//  UIDevice+SystemVersionNumber.h
+//  LMAlertViewDemo
+//
+//  Created by Lin Cheng Kai on 14/4/24.
+//  Copyright (c) 2014年 Bestir Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIDevice (SystemVersionNumber)
+@property (nonatomic, readonly) float systemVersionNumber;
+
+@end
